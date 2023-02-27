ff9772837ce221ca25e827254ce123d1a255dc94
blackhaird <1652233944@qq.com>
2023-02-27 23:56:29 +0800

    完成第二页面的布局

--- src/main_page/src/main.vue
+++ src/main_page/src/main.vue
@@ -45,14 +45,47 @@
           <div class="div_main_ID_involve_display row ">
             <Main_IntroductionDirection v-for="list in IntroductionDirection_lists" :message_txt="list.message"
                                         :icon_locate="list.icon_locate" :key="list" class="col"
-                                        :class="{bottomUp_Start:true,bottomUp_End:ID_show}" />
+                                        :class="{bottomUp_Start:true,bottomUp_End:ID_show}"/>
 
           </div>
         </div>
       </div>
 
       <!--  图片说明    -->
-      <div class="row test_box" ref="test_box"></div>
+      <div class="row div_main_second_panel_display" ref="second_panel">
+        <div class="row"></div>
+        <!--        共用Title-->
+        <div class="div_main_item_title ">
+          <h1>关于<span>项目</span></h1>
+          <h3>Item <span>Description </span></h3>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 div_main_second_panel_img_display ">
+            <div class="div_main_second_panel_img_first"
+                 :class="{div_main_second_panel_img_div_first:second_panel_img_div_show}">
+              <img src="./assets/main_second_panel_img.png" class="img_main_second_panel_img_first_start"
+                   :class="{img_main_second_panel_img_first:second_panel_img_show}">
+            </div>
+          </div>
+
+          <div class="col-lg-4 div_main_second_panel_message_display">
+            <div style="text-align: center">
+              <h1 class="row">阳光学院简章</h1>
+              <p class="row">
+                Irregular whitespace not allowed no-irregular-whitespace<br>
+                Irregular whitespace not allowed no-irregular-whitespace<br>
+                Irregular whitespace not allowed no-irregular-whitespace<br>
+                Irregular whitespace not allowed no-irregular-whitespace<br>
+                Irregular whitespace not allowed no-irregular-whitespace<br>
+                Irregular whitespace not allowed no-irregular-whitespace<br>
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
     </div>
   </div>
@@ -80,13 +113,18 @@ export default {
     const currentScroll = ref(0);
     const vm = getCurrentInstance();
     const ID_show = ref(false);
-
+    const second_panel_img_show = ref(false)
+    const second_panel_img_div_show = ref(false)
     const handleScroll = () => {
       currentScroll.value = window.pageYOffset;
-      console.log(currentScroll.value);
       if (currentScroll.value >= vm.ctx.$refs.main_item.offsetTop - 800) {
-        console.log("success");
         ID_show.value = true;
+      }
+      if (currentScroll.value >= vm.ctx.$refs.second_panel.offsetTop - 500) {
+        second_panel_img_div_show.value = true
+        setTimeout(() => {
+          second_panel_img_show.value = true;
+        }, 400)
       }
     }
 
@@ -113,6 +151,8 @@ export default {
       currentScroll,
       handleScroll,
       ID_show,
+      second_panel_img_div_show,
+      second_panel_img_show,
     }
   }
 }
@@ -197,15 +237,13 @@ main_homepage的css样式设定
   position: relative;
 
   height: 50vh;
-  background: blue;
 }
 
 .div_main_ID_involve_display {
   position: relative;
   margin: 0 auto;
-  width: 170vh;
+  width: 150vh;
   height: 50vh;
-  background: red;
 }
 
 .div_main_item_title h1 {
@@ -218,5 +256,51 @@ main_homepage的css样式设定
 }
 
 /*图片说明面板*/
+.div_main_second_panel_display {
+  width: 100%;
+  height: 100vh;
+  background: rebeccapurple;
+}
 
+.div_main_second_panel_img_display {
+  display: flex;
+  align-items: center;
+  width: 25%;
+  height: 80vh;
+}
+
+.div_main_second_panel_message_display {
+  width: 75%;
+  height: 80vh;
+  background: rosybrown;
+}
+
+.div_main_second_panel_img_first {
+  opacity: 0;
+  width: 100%;
+  height: 80vh;
+  background: var(--color-button_color_blue);
+  overflow: hidden;
+}
+
+.img_main_second_panel_img_first {
+  animation: second_panel_show 1s;
+  opacity: 1 !important;
+}
+
+.div_main_second_panel_img_div_first {
+  animation: second_panel_show 0.5s;
+  opacity: 1 !important;
+}
+
+.img_main_second_panel_img_first_start {
+  margin-left: 20px;
+  margin-top: 20px;
+  position: relative;
+  max-width: 100%;
+  width: 100%;
+  opacity: 0;
+  display: block;
+  box-shadow: var(--color-font_gray) -5px 10px 10px 1px;
+}
 </style>
